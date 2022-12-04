c0425bb1f6b9bb94911526fc5919e0acf808044c
Jonathan <jonathan.gualli@epn.edu.ec>
2022-12-04 15:50:03 -0500

    Se creo el metodo RegistrarNuevosEstudiantes

--- src/registrouniversidadgit/Operaciones.java
+++ src/registrouniversidadgit/Operaciones.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package registrouniversidadgit;
 
-/**
- *
- * @author Jonathan
- */
+import java.util.Scanner;
+
 public class Operaciones {
-    
+
+    //Nombre de las Asignaturas están predefinidas.
+    private static final String[] ASIGNATURAS = new String[]{"Matematicas",
+        "Historia", "Ciencias", "Literatura", "Informatica"};
+    //Array de Estudiantes
+    private static Estudiante[] estudiantes;
+
+    private static Scanner teclado = new Scanner(System.in);
+
+    public void registrarNuevosEstudiantes() {
+
+        //Inicializamos array de Estudiantes
+        estudiantes = new Estudiante[2];
+
+        //Por cada Estudiante, registraremos 5 Asignaturas, con 3 notas parciales
+        for (int est = 0; est < estudiantes.length; est++) {
+            System.out.println("\n\nRegistrando Estudiante #" + (est + 1));
+            System.out.print("Nombre: ");
+            String nombre = teclado.nextLine();
+            //Registramos Estudiante en el array
+            estudiantes[est] = new Estudiante(nombre);
+
+            //Para este Estudiante, registramos ahora 5 Asignaturas
+            for (int asig = 0; asig < ASIGNATURAS.length; asig++) {
+                //Construimos la Asignatura, tomando el nombre del array predefinido
+                Asignatura asignatura = new Asignatura(ASIGNATURAS[asig]);
+
+                //Ahora, hay que pedir 3 notas parciales para esta Asignatura
+                System.out.println("\nNotas parciales de " + ASIGNATURAS[asig]);
+                for (int i = 0; i < 3; i++) {
+                    System.out.print("Parcial #" + (i + 1) + ": ");
+                    int parcial = Integer.parseInt(teclado.nextLine());
+                    //Registramos parcial en la Asignatura, indicando la posicion donde debe registrarse
+                    asignatura.registrarNotaParcial(parcial, i);
+                }
+
+                /*
+				 * Ya tenemos Asignatura con las 3 notas parciales.
+				 * La registramos en el Estudiante, indicando la posición donde registrarse
+                 */
+                estudiantes[est].registrarAsignatura(asignatura, asig);
+            }
+            //Aqui termina el bucle para registrar 5 Asignaturas.
+            //Ahora se volverá a repetir el ciclo para otro Estudiante, hasta completar los 5
+        }
+    }
 }
